07101d8f0f15a444e106397699000819d4dc6ad5
ProSonam <sonamsribastab@gmail.com>
2025-07-18 13:29:39 +0530

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,1 +1,13 @@
+from PIL import ImageEnhance
+
+def enhance_image(image):
+    # Enhance brightness
+    enhancer_brightness = ImageEnhance.Brightness(image)
+    image = enhancer_brightness.enhance(1.2)
+
+    # Enhance sharpness
+    enhancer_sharpness = ImageEnhance.Sharpness(image)
+    image = enhancer_sharpness.enhance(1.5)
+
+    return image
 
